abfe30a7353be2e21bc5d690ecfd856c49aef423
Tah Ferreira <tahferreira8@gmail.com>
2025-04-30 13:44:58 -0300

    Código/pseudocódigo

---
+++ ContagemRegressiva.java
@@ -0,0 +1,20 @@
+package Desafios;
+
+public class ContagemRegressiva {
+	public static void main(String[] args) {
+		
+		int numero = 100;
+
+		System.out.println("Contagem regressiva de 100 até 0, pulando de 10 em 10:");
+
+		while (numero >= 0) {
+		System.out.println(numero);
+		numero -= 10;
+		        }
+
+		System.out.println("Fim da contagem!");
+		    }
+		
+	}
+
+
