d95c265f0ff1d4328614ac5fabaed265560fb43e
Tah Ferreira <tahferreira8@gmail.com>
2025-04-30 12:26:43 -0300

    Código/pseudocódigo

---
+++ TabuadaInterativa.java
@@ -0,0 +1,26 @@
+package Desafios;
+
+import java.util.Scanner;
+
+public class TabuadaInterativa {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Digite um número para ver sua tabuada: ");
+		int numero = scanner.nextInt();
+
+		System.out.println("Tabuada do " + numero + ":");
+
+		for (int i = 1; i <= 10; i++) {
+		int resultado = numero * i;
+		
+		System.out.println(numero + " x " + i + " = " + resultado);
+		        }
+
+		scanner.close();
+		    }
+		
+	}
+
+
